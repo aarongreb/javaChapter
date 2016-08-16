8b0115b10dadb289c0650992082c1526ade7527a
Aaron Greb <aaron.greb@appian.com>
2016-08-16 06:31:46 -0400

    Added LocalDateTime exercises

---
+++ Exercises/src/javaChapter/LocalDateTimeExercises.java
@@ -0,0 +1,39 @@
+package javaChapter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeExercises {
+  public static void main(String[] args) {
+    exercise1();
+
+    exercise2();
+
+    exercise3();
+
+    exercise4();
+  }
+
+  // Display the current time but for tomorrow.
+  private static void exercise1() {
+    /* Your solution goes here */
+  }
+
+  // Display today's date from last year.
+  private static void exercise2() {
+    /* Your solution goes here */
+  }
+
+  // Using a Period, display your 50th birthday.
+  private static void exercise3() {
+    /* Your solution goes here */
+  }
+
+  // Display your start date in the format MM/DD/YYYY
+  private static void exercise4() {
+    /* Your solution goes here */
+  }
+}
